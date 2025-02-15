a78621f0f1f37ea07d45b41997f67ac952837285
Martin Traverse <martin.traverse@icloud.com>
2025-02-15 00:55:11 +0000

    Avro producers - interface and base class

---
+++ adapter/avro/src/main/java/org/apache/arrow/adapter/avro/producers/Producer.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.arrow.adapter.avro.producers;
+
+import org.apache.arrow.vector.FieldVector;
+import org.apache.avro.io.Encoder;
+
+import java.io.IOException;
+
+public interface Producer<T extends FieldVector> extends AutoCloseable {
+
+  /**
+   * Produce a specific type value from the vector and write it to avro encoder.
+   *
+   * @param encoder avro encoder to write data
+   * @throws IOException on error
+   */
+  void produce(Encoder encoder) throws IOException;
+
+  /**
+   * Close this produce
+   */
+  @Override
+  void close() throws Exception;
+
+  /**
+   * Reset the vector within producer
+   *
+   * @return true if reset is successful, false if reset is not needed.
+   */
+  boolean resetValueVector(T vector);
+
+  /**
+   * Get the vector within the producer.
+   */
+  FieldVector getVector();
+}
